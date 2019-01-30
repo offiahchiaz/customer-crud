74f0ff956f5e92c8c52758f47e97a5466e104b32
Offiahchiaz <offiahchiaz@gmail.com>
2019-01-30 14:34:00 +0100

    Add updateCustomer handler

--- schema.js
+++ schema.js
@@ -84,6 +84,19 @@ const mutation = new GraphQLObjectType({
                return axios.delete(`http://localhost:3000/customers/${args.id}`)
                 .then((res) => res.data); 
             }
+        },
+        updateCustomer: {
+            type: CustomerType,
+            args: {
+                id: {type: new GraphQLNonNull(GraphQLString)},
+                name: {type: GraphQLString},
+                email: {type: GraphQLString},
+                age: {type: GraphQLInt}
+            },
+            resolve(parentValue, args) {
+                return axios.patch(`http://localhost:3000/customers/${args.id}`, args)
+                    .then((res) => res.data); 
+            }
         }
     }
 });
